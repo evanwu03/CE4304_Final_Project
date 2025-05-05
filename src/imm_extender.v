9e9ad2e5f3857a2f4a68af17929177520a16c037
evanwu03 <ewu43951@gmail.com>
2025-05-05 18:24:32 -0500

    Immediate extender complete, covers R, I, and J type cases

--- src/imm_extender.v
+++ src/imm_extender.v
@@ -16,14 +16,49 @@
 //  Outputs:  
 //  o_imm_ext - extended immediate field
 
+
+`include "opcode_defs.vh"
+
 module imm_extender #(
     parameter DATA_WIDTH = 36,
     parameter SELECT_WIDTH = 2,
-    parameter IMM_MAX_WIDTH = 14
+    parameter IMM_MAX_WIDTH = 14,
+    parameter I_IMM_WIDTH = 8,
+    parameter J_IMM_WIDTH = 14
 ) (
     input [IMM_MAX_WIDTH-1:0] i_immRaw,
     input [SELECT_WIDTH-1:0] i_immSel,
     output reg [DATA_WIDTH-1:0] o_imm_ext
 );
-    
+
+always @(*) 
+    begin
+        
+        case (i_immSel) 
+            // R type instructions don't need to use immediate 
+            `IMMSEL_NONE: begin
+                o_imm_ext = {DATA_WIDTH{1'b0}}
+
+            end
+
+            // I type instructions 8 bit immediate field
+            `IMMSEL_I_TYPE: begin
+                o_imm_ext = {{(DATA_WIDTH-8){i_immRaw[I_IMM_WIDTH-1]}}, i_immRaw[I_IMM_WIDTH-1:0]}; 
+            end 
+
+            // J type instructions 14 bit immediate field
+            `IMMSEL_J_TYPE: begin
+                o_imm_ext = {{(DATA_WIDTH-14){i_immRaw[J_IMM_WIDTH-1]}}, i_immRaw[J_IMM_WIDTH-1:0]};              
+
+            end 
+
+            // Catch all do nothing with extended value
+            default: begin
+                o_imm_ext = {DATA_WIDTH{1'b0}};
+            end
+
+        endcase 
+    end
+
+
 endmodule
